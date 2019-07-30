6142a979171cabe0c039602ad39d1861230f7eb7
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2019-07-30 12:34:26 -0300

    Calculate everything

--- src/app/fuel/fuel.component.ts
+++ src/app/fuel/fuel.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit }  from '@angular/core';
-import { FuelType }           from '../fuelType';
+import { Component, OnInit }  from '@angular/core'
+import { FuelType }           from '../fuelType'
 
 @Component({
   selector: 'app-fuel',
@@ -10,30 +10,111 @@ export class FuelComponent implements OnInit {
 
   ethanol: FuelType = {
     name:     'Etanol',
-    price:    0.00,
-    consume:  0.00,
+    price:    null,
+    consume:  null,
     value:    null
-  };
+  }
 
   gasoline: FuelType = {
     name:     'Gasolina',
-    price:    0.00,
-    consume:  0.00,
+    price:    null,
+    consume:  null,
     value:    null
-  };
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  calculate(item) {
-    if(item.price > 0 && item.consume > 0){
-      item.value = item.price / item.consume;
-    }
+  run(){
+    this.ethanol.consume  = this.maskDecimal(this.ethanol.consume)
+    this.gasoline.consume = this.maskDecimal(this.gasoline.consume)
+
+    this.ethanol.price  = this.maskCurrency(this.ethanol.price)
+    this.gasoline.price = this.maskCurrency(this.gasoline.price)
+
+    this.calculate(this.ethanol)
+    this.calculate(this.gasoline)
   }
 
-  build() {
-    this.calculate(this.ethanol);
+  maskDecimal(value){
+    if(value == null){
+      return
+    }
+
+    value     = value.replace(/\D/g, '')
+    value     = value.replace(/^0+/g, '')
+    var size  = value.length
+
+    if(size < 3){
+      while(value.length < 2){
+        value = "0"+value
+      }
+      value = "0," + value
+
+      size  = value.length
+      value = value.replace(/\D/g, '')
+    }
+
+    var absolute  = value.substr(0, size - 2)
+    var decimal   = value.substr(size -2)
+    value         = absolute.toString() + "," + decimal.toString()
+
+    return value
+  }
+
+  maskCurrency(value){
+    if(value == null){
+      return
+    }
+    value         = this.padNumber(value)
+    var size      = value.length
+    var absolute  = value.substr(0, size - 2)
+    var decimal   = value.substr(size -2)
+    value         = absolute.toString() + "," + decimal.toString()
+
+    return value
+  }
+
+  padNumber(value){
+    if(value == null){
+      return
+    }
+
+    value     = value.replace(/\D/g, '')
+    value     = value.replace(/^0+/g, '')
+    var size  = value.length
+
+    switch(size){
+      case 0:
+        value = "000"
+        break
+      case 1:
+        value = "00" + value
+        break
+      case 2:
+        value = "0" + value
+        break
+    }
+
+    return value
+  }
+
+  calculate(item) {
+    if(item.price == null || item.consume == null){
+      return
+    }
+
+    // Cast from string to float
+    var price = parseFloat(item.price.replace(/,/, '.'))
+    var consume = parseFloat(item.consume.replace(/,/, '.'))
+
+    if(price > 0 && consume > 0){
+      item.value = price / consume
+    }
+    else {
+      item.value = 0
+    }
   }
 }
--- src/app/fuelType.ts
+++ src/app/fuelType.ts
@@ -2,6 +2,6 @@ export class FuelType {
   name:     string;
   price:    number;
   consume:  number;
-  value:    number;
+  value:    string;
 }
 
